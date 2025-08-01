8db8f06a8947f72d43b13f72f865f4133eaeb4cd
AlexBritton402 <alexbritton303@gmail.com>
2025-08-01 08:27:28 +0000

    Fix Cesium init + add access token

--- frontend/viewer.js
+++ frontend/viewer.js
@@ -1,4 +1,4 @@
-Cesium.Ion.defaultAccessToken = 'YOUR_CESIUM_ION_ACCESS_TOKEN';
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5YzkyNjFlNy1lY2JkLTRlMzMtOTIwNi02OTAxM2NmMWFlMDEiLCJpZCI6MzI3NjAwLCJpYXQiOjE3NTQwMDM5ODV9.eIlC7h0S8AQvHlZK9vd_AvQ_QMuARyFEKbEJRfQn4jk';
 
 const viewer = new Cesium.Viewer('cesiumContainer', {
   terrain: Cesium.createWorldTerrain(),
