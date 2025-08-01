8b74314b4b7ee74dae7c9d43ccfe1b0742eb4c67
AlexBritton402 <alexbritton303@gmail.com>
2025-08-01 15:39:06 +0000

    Working Cesium viewer setup

--- frontend/viewer.js
+++ frontend/viewer.js
@@ -1,7 +1,7 @@
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NjA0Mjc4MC1kOTE1LTRlZWEtYThlZC0xZDEwYzVlZjIwYzciLCJpZCI6MzI3NjAwLCJpYXQiOjE3NTQwMzgwMzd9.93TS4XH08s33P0tkINAlek26auqBlVFjIaNVc1JN3Ag';
 
 const viewer = new Cesium.Viewer('cesiumContainer', {
-  terrainProvider: Cesium.createWorldTerrain(),
+  terrain: Cesium.createWorldTerrain(),
   baseLayerPicker: false,
   animation: false,
   timeline: false
